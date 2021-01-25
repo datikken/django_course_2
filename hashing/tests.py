9e22b77b8fab97e5d2fb47653abcd0dd187e6b10
datikken <tikken23@gmail.com>
2021-01-25 04:11:23 +0300

    initial site with tdd

---
+++ hashing/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from selenium import webdriver
+
+# class FunctionalTestCase(TestCase):
+    # def setUp(self):
+    #     self.browser = webdriver.Firefox()
+    #
+    # def test_home_page_response(self):
+    #     self.browser.get('http://localhost:8000')
+    #     self.assertIn('install', self.browser.page_source)
+    #
+    # def test_home_page_has_input(self):
+    #     self.browser.get('http://localhost:8000')
+    #     text = self.browser.find_element_by_id('user_input')
+    #     text.send_keys('hello')
+    #     self.browser.find_element_by_name('submit').click()
+    #     self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+    #
+    # def tearDown(self):
+    #     self.browser.quit()
+
+class UnitTestCase(TestCase):
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')

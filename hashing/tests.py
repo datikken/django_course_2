7a7e1bea034cc32b6abe2c7af31c4a83bf142fe4
datikken <tikken23@gmail.com>
2021-01-25 04:55:13 +0300

    testing form testing hash func

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
-
+import hashlib
 
 # class FunctionalTestCase(TestCase):
     # def setUp(self):
@@ -16,7 +16,7 @@ from .forms import HashForm
     #     text = self.browser.find_element_by_id('user_input')
     #     text.send_keys('hello')
     #     self.browser.find_element_by_name('submit').click()
-    #     self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824', self.browser.page_source)
+    #     self.assertIn('', self.browser.page_source)
     #
     # def tearDown(self):
     #     self.browser.quit()
@@ -25,6 +25,11 @@ class UnitTestCase(TestCase):
     def test_home_homepage_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
     def test_hash_form(self):
         form = HashForm(data={'text': 'hello'})
         self.assertTrue(form.is_valid())
+
+    def test_hash_farsh(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
